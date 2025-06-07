7f25e349b0e7c7eedad4cb46ce12fa663c8b8f2f
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2025-06-07 08:30:02 +0530

    Create class.dart

---
+++ class.dart
@@ -0,0 +1,18 @@
+class demo
+{
+ 
+    void display()
+    {
+        print("Hello from  Display");
+    }
+    
+}
+
+
+int main()
+{
+    print("Inside main");
+    demo d = new demo();
+    d.display();
+    return 0;
+}
